ae55544ba960e9d480791f8cd9e740b0427a1a5b
Álvaro Carmona <104690058+alvarocp4@users.noreply.github.com>
2024-01-25 11:49:31 +0100

    Update streamlit_app.py
    added prepopulate

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #LIST TO PICK
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 #DISPLAY TABLE
 streamlit.dataframe(my_fruit_list)
