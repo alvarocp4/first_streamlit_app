a50acf3d9b1c9a99d0ada33ae8003f72d14bcd51
Álvaro Carmona <104690058+alvarocp4@users.noreply.github.com>
2024-01-25 11:55:40 +0100

    Update streamlit_app.py
    bit  hanges

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #LIST TO PICK
 fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruit_selected  = my_fruit_list.loc[fruit_selected]
+fruits_to_show  = my_fruit_list.loc[fruit_selected]
 #DISPLAY TABLE
-streamlit.dataframe(fruit_selected )
+streamlit.dataframe(fruit_selected ) = streamlit.dataframe(fruits_to_show )
