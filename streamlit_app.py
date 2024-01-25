802d4e5c969bb4a8fe993bea1b5426f93be52d04
Álvaro Carmona <104690058+alvarocp4@users.noreply.github.com>
2024-01-25 11:47:28 +0100

    Update streamlit_app.py
    set index fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 #TABLES
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #BODY
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breaksfast Favorites')
