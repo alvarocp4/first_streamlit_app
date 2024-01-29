e616b645b47e6caa01a76bb4c477b2d97bfb9551
Álvaro Carmona <104690058+alvarocp4@users.noreply.github.com>
2024-01-29 17:13:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,7 @@ try:
 
 
 #Hellow from snowflake/CONNECTOR
+streamlit.error(
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -52,4 +53,4 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit= streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 #Add new excute insert into
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+my_cur.execute("insert into fruit_load_list values('from streamlit')"))
